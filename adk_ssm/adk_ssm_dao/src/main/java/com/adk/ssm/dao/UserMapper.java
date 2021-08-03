e8d00e1fdb3950231987b70113e776d115c36878
ADKcode <790532173@qq.com>
2021-08-04 00:06:31 +0800

    添加用户登录验证
    添加一小部分用户验证功能

--- adk_ssm/adk_ssm_dao/src/main/java/com/adk/ssm/dao/UserMapper.java
+++ adk_ssm/adk_ssm_dao/src/main/java/com/adk/ssm/dao/UserMapper.java
@@ -5,7 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserMapper {
-
-
     public UserInfo findByUsername(String username) throws Exception;
 }
--- adk_ssm/adk_ssm_service/src/main/java/com/adk/ssm/service/UserServiceImpl.java
+++ adk_ssm/adk_ssm_service/src/main/java/com/adk/ssm/service/UserServiceImpl.java
@@ -2,12 +2,17 @@ package com.adk.ssm.service;
 
 import com.adk.ssm.dao.UserMapper;
 import com.adk.ssm.domain.UserInfo;
+import javafx.beans.property.SimpleListProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("userService")
 @Transactional
@@ -23,7 +28,13 @@ public class UserServiceImpl implements IUserService{
             e.printStackTrace();
         }
         //通过他的User对象封装成userdetails 并返回
-        User user=new User(userInfo.getUsername(),userInfo.getPassword(),null);
+        User user=new User(userInfo.getUsername(),userInfo.getPassword(),getAuthority());
         return user;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthority(){
+        List<SimpleGrantedAuthority> list=new ArrayList<>();
+        list.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return list;
     }
 }
